6234a4b9cdd29007e4360652bcc753165dfa2fe4
unais ali <51183141+unaisshazan@users.noreply.github.com>
2020-07-11 21:41:29 +0500

    add unais

--- src/Navbar.js
+++ src/Navbar.js
@@ -30,7 +30,7 @@ export default function ButtonAppBar() {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" className={classes.title}>
-            News
+           UNAIS
           </Typography>
           <Button color="inherit">Login</Button>
         </Toolbar>
