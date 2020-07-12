e767c24e64bba4777fd9e82eee9a6f123f26818b
unais ali <51183141+unaisshazan@users.noreply.github.com>
2020-07-12 19:04:26 +0500

    add unais

--- src/Navbar.js
+++ src/Navbar.js
@@ -6,6 +6,7 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import Drawer from './Drawer';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -27,7 +28,7 @@ export default function ButtonAppBar() {
       <AppBar position="static">
         <Toolbar>
           <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
+           <Drawer/>
           </IconButton>
           <Typography variant="h6" className={classes.title}>
            UNAIS
